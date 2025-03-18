1c9315605dcf83bdd9b7a7d95724687b0f9cf893
Евеліна <evelina.bashynska.pz.2023@lpnu.ua>
2025-03-18 18:11:32 +0000

    Update petProject.cpp
    Added extra functions and fixed bugs.

--- petProject.cpp
+++ petProject.cpp
@@ -1,12 +1,27 @@
 #include "header.h"
-#include "source.cpp"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <destination>\n", argv[0]);
-        return EXIT_FAILURE;
+    int max_hops = 30;
+    int timeout = 3;
+    char *interface = NULL;
+    
+    int opt;
+    while ((opt = getopt(argc, argv, "m:t:i:")) != -1) {
+        switch (opt) {
+            case 'm': max_hops = atoi(optarg); break;
+            case 't': timeout = atoi(optarg); break;
+            case 'i': interface = optarg; break;
+            default:
+                fprintf(stderr, "Usage: %s [-m max_hops] [-t timeout] [-i interface] <destination>\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
     
-    trace_route(argv[1]);
+    if (optind >= argc) {
+        fprintf(stderr, "Missing destination argument\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    trace_route(argv[optind], max_hops, timeout, interface);
     return EXIT_SUCCESS;
 }
