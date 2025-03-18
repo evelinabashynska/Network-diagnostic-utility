b011a1ec2c3eea9f81c8dd891fca68219c02ebd7
Евеліна <evelina.bashynska.pz.2023@lpnu.ua>
2025-03-18 18:12:26 +0000

    Update source.cpp
    Added extra functions.

--- source.cpp
+++ source.cpp
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void trace_route(const char *destination) {
+void trace_route(const char *destination, int max_hops, int timeout, const char *interface) {
     printf("Tracing route to %s...\n", destination);
     
     int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
@@ -9,25 +9,32 @@ void trace_route(const char *destination) {
         exit(EXIT_FAILURE);
     }
     
-    struct sockaddr_in target_addr;
-    memset(&target_addr, 0, sizeof(target_addr));
-    target_addr.sin_family = AF_INET;
-    target_addr.sin_addr.s_addr = inet_addr(destination);
+    if (interface) {
+        if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, interface, strlen(interface)) < 0) {
+            perror("Failed to bind to network interface");
+            exit(EXIT_FAILURE);
+        }
+    }
     
-    struct hostent *host_info = gethostbyname(destination);
-    if (host_info) {
+    struct sockaddr_in target_addr = {0};
+    target_addr.sin_family = AF_INET;
+    if (inet_aton(destination, &target_addr.sin_addr) == 0) {
+        struct hostent *host_info = gethostbyname(destination);
+        if (!host_info) {
+            fprintf(stderr, "Failed to resolve host: %s\n", destination);
+            exit(EXIT_FAILURE);
+        }
         target_addr.sin_addr = *((struct in_addr *)host_info->h_addr);
         printf("Resolved %s to %s\n", destination, inet_ntoa(target_addr.sin_addr));
     }
     
-    for (int ttl = 1; ttl <= MAX_HOPS; ttl++) {
+    for (int ttl = 1; ttl <= max_hops; ttl++) {
         if (setsockopt(sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
             perror("Failed to set TTL");
             exit(EXIT_FAILURE);
         }
         
-        struct icmphdr icmp_hdr;
-        memset(&icmp_hdr, 0, sizeof(icmp_hdr));
+        struct icmphdr icmp_hdr = {0};
         icmp_hdr.type = ICMP_ECHO;
         icmp_hdr.un.echo.id = getpid();
         icmp_hdr.un.echo.sequence = ttl;
@@ -46,13 +53,11 @@ void trace_route(const char *destination) {
         char buffer[PACKET_SIZE];
         
         fd_set readfds;
-        struct timeval timeout;
+        struct timeval timeout_val = {timeout, 0};
         FD_ZERO(&readfds);
         FD_SET(sock, &readfds);
-        timeout.tv_sec = TIMEOUT;
-        timeout.tv_usec = 0;
         
-        if (select(sock + 1, &readfds, NULL, NULL, &timeout) > 0) {
+        if (select(sock + 1, &readfds, NULL, NULL, &timeout_val) > 0) {
             ssize_t bytes_received = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&recv_addr, &addr_len);
             if (bytes_received > 0) {
                 gettimeofday(&end, NULL);
@@ -62,13 +67,11 @@ void trace_route(const char *destination) {
                 struct hostent *host = gethostbyaddr(&recv_addr.sin_addr, sizeof(recv_addr.sin_addr), AF_INET);
                 const char *hostname = host ? host->h_name : "Unknown";
                 
-                if (icmp_reply->type == ICMP_TIME_EXCEEDED) {
-                    printf("Hop %d: %s (%s) (Time Exceeded), time=%ld ms\n", ttl, inet_ntoa(recv_addr.sin_addr), hostname, elapsed_time);
-                } else if (icmp_reply->type == ICMP_ECHOREPLY) {
-                    printf("Hop %d: %s (%s) (Destination Reached), time=%ld ms\n", ttl, inet_ntoa(recv_addr.sin_addr), hostname, elapsed_time);
+                printf("Hop %d: %s (%s), time=%ld ms\n", ttl, inet_ntoa(recv_addr.sin_addr), hostname, elapsed_time);
+                
+                if (icmp_reply->type == ICMP_ECHOREPLY) {
+                    printf("Destination reached!\n");
                     break;
-                } else {
-                    printf("Hop %d: %s (%s) (Unexpected response), time=%ld ms\n", ttl, inet_ntoa(recv_addr.sin_addr), hostname, elapsed_time);
                 }
             }
         } else {
@@ -79,7 +82,6 @@ void trace_route(const char *destination) {
     
     close(sock);
 }
-
 unsigned short checksum(void *b, int len) {
     unsigned short *buf = (unsigned short*)b;
     unsigned int sum = 0;
